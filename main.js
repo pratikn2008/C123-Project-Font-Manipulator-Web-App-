5d75d3c32493c4bb33551990f8f226f44a0d16f1
Pratik Nayak <104261131+pratikn2008@users.noreply.github.com>
2022-11-20 11:54:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(480, 440);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    background('#969A97');
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized!')
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
